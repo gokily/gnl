9277b8519197599616d0f9f67843b001649b0b9d
Goki LY <gly@student.42.fr>
2018-11-11 16:01:42 +0100

    Change the file to get the tests to pass but it's doesn't work

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,42 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 13:21:17 by gly               #+#    #+#             */
-/*   Updated: 2018/11/11 10:35:13 by gly              ###   ########.fr       */
+/*   Updated: 2018/11/11 16:01:02 by gly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft.h"
 
-char	*ft_strjoinfree(char *s1, char const *s2)
+char	*ft_strjoinfree(char const *s1, char const *s2)
 {
-	char	*pt;
+	char	*ptr;
+	int		len;
+	int		i;
+	int		j;
 
-	if (!(pt = ft_strjoin(s1, s2)))
+	if (s2 == 0)
 		return (0);
-	free(s1);
-	return (pt);
+	puts("IN STRJOINFREE");
+	printf("s1 is %s and s2 is %s\n", s1, s2);
+	len = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	if (!(ptr = ft_strnew(len)))
+		return (0);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (i < len)
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+	}
+	free((void *)s1);
+	return (ptr);
 }
 
 t_fdlst	*ft_lstsrc_fd(t_fdlst **lst_fdrst, int fd)
@@ -56,17 +77,25 @@ int		ft_writeline(char *read, char **line, t_fdlst *lst_elem)
 
 	if ((pt = ft_strchr(read, '\n')))
 	{
+		puts("found a \\n !!");
+		printf("line is %s\n", *line);
+		*pt = '\0';
 		if (!(lst_elem->rst = ft_strdup(pt + sizeof(char))))
 			return (-1);
-		if (!(*line = ft_strjoinfree(*line, ft_strsub(read, 0, pt - read))))
-		{
-			free(lst_elem->rst);
+		printf("The new rest is %s\n", lst_elem->rst);
+		printf("Read is %s\n", read);
+		printf("Line now is %s\n", *line);
+		if (!(*line = ft_strjoinfree(*line, read)))
 			return (-1);
-		}
+		printf("The new line is %s\n", *line);
+
 		return (1);
 	}
 	if (!(*line = ft_strjoinfree(*line, read)))
 		return (-1);
+	ft_strclr(lst_elem->rst);
+	free(lst_elem->rst);
+	lst_elem->rst = 0;
 	return (0);
 }
 
@@ -78,14 +107,20 @@ int		get_next_line(const int fd, char **line)
 	t_fdlst			*lst_elem;
 	int				n;
 
-	if (fd < 0 || line == NULL)
+	if (fd < 0 || line == NULL || read(fd, NULL, 0) < 0 )
 		return (-1);
-	*line = (char *)malloc(sizeof(char *));
 	if (!(lst_elem = ft_lstsrc_fd(&lst_fdrst, fd)))
 		return (-1);
+	ft_strclr(*line);
 	if (lst_elem->rst != 0 && ft_strlen(lst_elem->rst))
 	{
+		puts("THERE IS A RST");
+		ft_putstr("rst is ");
+		ft_putnbr(ft_strlen(lst_elem->rst));
+		ft_putstr(" char long and is: ");
+		ft_putendl(lst_elem->rst);
 		n = ft_writeline(lst_elem->rst, line, lst_elem);
+		printf("n is %i\n", n);
 		if (n != 0)
 			return (n);
 	}
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,18 @@
 /*   By: gly <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 12:54:51 by gly               #+#    #+#             */
-/*   Updated: 2018/11/11 10:18:33 by gly              ###   ########.fr       */
+/*   Updated: 2018/11/11 15:27:17 by gly              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 31
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include <stdio.h>
 
 typedef struct		s_fdlst
 {
